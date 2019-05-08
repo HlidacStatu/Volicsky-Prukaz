19519b63350f27757180c3dd2c55767f32234d67
Kamil Zmeskal <kamil.zm@seznam.cz>
2019-05-08 15:24:54 +0200

    Refactoring - move /assets/files/appversion.txt to root - add config file for voting days - refacfor form (only) for independ on language - new validation system

---
+++ appconfig.js
@@ -0,0 +1,18 @@
+const appConfig = {
+    votingDays: [
+        {
+            date: "24.05.2019",
+            time: "14:00-22:00"
+        },
+        {
+            date: "25.05.2019",
+            time: "08:00-14:00"
+        }],
+    votingCardRequestdeliveryLastTime:
+    {
+        personal: "22.05.2019 16:00",
+        post: "17.05.2019 16:00"
+    },
+    latestBornDate: "24.05.2001",     // last born date for validation
+    showFullValidationSummary: false,  // true = show all error, false = show only important info
+}
