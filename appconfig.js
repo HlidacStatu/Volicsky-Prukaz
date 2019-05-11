153c911e893b15df7b02bd7cbeda0d6f0dddc688
Kamil Zmeskal <kamil.zm@seznam.cz>
2019-05-11 20:47:04 +0200

    Fixes from code review + another modification and (maybe)improvement - format date and time base on language - localise election name in header - some texts corrections - new properties in config file

--- appconfig.js
+++ appconfig.js
@@ -1,18 +1,21 @@
 const appConfig = {
-    votingDays: [
-        {
-            date: "24.05.2019",
-            time: "14:00-22:00"
-        },
-        {
-            date: "25.05.2019",
-            time: "08:00-14:00"
-        }],
-    votingCardRequestdeliveryLastTime:
+    electionName: {
+        'CZ': 'Volby do Evropského parlamentu 2019',
+        'EN': 'European Parliament election 2019'},
+    electionDays: [{
+            startTime: "24.05.2019 14:00",  // in one election day start and end date (not time) must be same
+            endTime: "24.05.2019 22:00"
+        },{
+            startTime: "25.05.2019 08:00",  // in one election day start and end date (not time) must be same
+            endTime: "25.05.2019 14:00"}],
+    voterCardRequestdeliveryLastTime:
     {
         personal: "22.05.2019 16:00",
         post: "17.05.2019 16:00"
     },
-    latestBornDate: "24.05.2001",     // last born date for validation
-    showFullValidationSummary: false,  // true = show all error, false = show only important info
+    minimumAge: 18,         // minimum age (year) for the latest election day
+    minimumDaysForRegisterOnElectoralRollBeforeElection: 40,         //  apply to have your details transferred (or register) to the EP electoral roll no later than 40 days before the election
+    applicationTemplate: "assets/files/Zadost-Volicsky-Prukaz-EU2019-CR.pdf",
+    filenameForDownloadApplication: "Zadost-Volicsky-Prukaz-EU2019-CR.pdf",
+    showFullValidationSummary: false        // true = show all error, false = show only important info
 }
